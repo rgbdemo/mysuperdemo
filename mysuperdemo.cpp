56b43834d9b6ac7004e9c248cebb8d085b2997c3
Nicolas Burrus <nicolas.burrus@manctl.com>
2012-06-30 01:06:51 -0700

    Update sample to use openni.

--- mysuperdemo.cpp
+++ mysuperdemo.cpp
@@ -1,59 +1,101 @@
-#include <ntk/camera/kinect_grabber.h>
-#include <ntk/camera/rgbd_processor.h>
-#include <ntk/utils/opencv_utils.h>
+/**
+ * This file is part of the nestk library.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Nicolas Burrus <nicolas.burrus@uc3m.es>, (C) 2010
+ */
 
-using namespace ntk;
+#include <ntk/ntk.h>
+#include <ntk/utils/debug.h>
+#include <ntk/camera/openni_grabber.h>
+#include <ntk/gesture/body_event.h>
+
+#include <QApplication>
+#include <QDir>
+#include <QMutex>
+
 using namespace cv;
+using namespace ntk;
 
-int main()
+namespace opt
 {
-  KinectGrabber grabber;
-  grabber.initialize();
-  // Set camera tilt.
-  grabber.setTiltAngle(0);
-  grabber.start();
+ntk::arg<bool> high_resolution("--highres", "High resolution color image.", 0);
+ntk::arg<int> kinect_id("--kinect-id", "Kinect id", 0);
+}
 
-  // Postprocess raw kinect data.
-  // Tell the processor to transform raw depth into meters using baseline-offset technique.
-  RGBDProcessor processor;
-  processor.setFilterFlag(RGBDProcessor::ComputeKinectDepthBaseline, true);
+int main(int argc, char **argv)
+{
+    // Parse command line options.
+    arg_base::set_help_option("-h");
+    arg_parse(argc, argv);
 
-  // OpenCV windows.
-  namedWindow("color");
-  namedWindow("depth");
-  namedWindow("depth_as_color");
-  
-  // Current image. An RGBDImage stores rgb and depth data.
-  RGBDImage current_frame;
-  while (true)
-  {
-    grabber.waitForNextFrame();
-    grabber.copyImageTo(current_frame);
-    processor.processImage(current_frame);
+    // Set debug level to 1.
+    ntk::ntk_debug_level = 1;
 
-    // Show the frames per second of the grabber
-    int fps = grabber.frameRate();
-    cv::putText(current_frame.rgbRef(),
-                cv::format("%d fps", fps),
-                Point(10,20), 0, 0.5, Scalar(255,0,0,255));
+    // Set current directory to application directory.
+    // This is to find Nite config in config/ directory.
+    QApplication app (argc, argv);
+    QDir::setCurrent(QApplication::applicationDirPath());
 
-    // Display the color image
-    imshow("color", current_frame.rgb());
+    // Declare the global OpenNI driver. Only one can be instantiated in a program.
+    OpenniDriver ni_driver;
 
-    // Show the depth image as normalized gray scale
-    imshow_normalized("depth", current_frame.depth());
+    // Declare the frame grabber.
+    OpenniGrabber grabber(ni_driver, opt::kinect_id());
 
-    // Compute color encoded depth.
-    cv::Mat3b depth_as_color;
-    compute_color_encoded_depth(current_frame.depth(), depth_as_color);
-    imshow("depth_as_color", depth_as_color);
+    // High resolution 1280x1024 RGB Image.
+    if (opt::high_resolution())
+        grabber.setHighRgbResolution(true);
 
-    // Enable switching to InfraRead mode.
-    unsigned char c = cv::waitKey(10) & 0xff;
-    if (c == 'q')
-      exit(0);
-  }
-  
- 
-  return 0;
+    // Start the grabber.
+    grabber.connectToDevice();
+    grabber.start();
+
+    // Holder for the current image.
+    RGBDImage image;
+
+    // Image post processor. Compute mappings when RGB resolution is 1280x1024.
+    OpenniRGBDProcessor post_processor;
+
+    namedWindow("depth");
+    namedWindow("color");
+    namedWindow("users");
+
+    char last_c = 0;
+    while (true && (last_c != 27))
+    {
+        // Wait for a new frame, get a local copy and postprocess it.
+        grabber.waitForNextFrame();
+        grabber.copyImageTo(image);
+        post_processor.processImage(image);
+
+        // Prepare the depth view, mapped onto rgb frame.
+        cv::Mat1b debug_depth_img = normalize_toMat1b(image.mappedDepth());
+
+        // Prepare the color view with skeleton and handpoint.
+        cv::Mat3b debug_color_img;
+        image.rgb().copyTo(debug_color_img);
+
+        // Prepare the user mask view as colors.
+        cv::Mat3b debug_users;
+        image.fillRgbFromUserLabels(debug_users);
+
+        imshow("depth", debug_depth_img);
+        imshow("color", debug_color_img);
+        imshow("users", debug_users);
+        last_c = (cv::waitKey(10) & 0xff);
+    }
+    grabber.stop();
 }
